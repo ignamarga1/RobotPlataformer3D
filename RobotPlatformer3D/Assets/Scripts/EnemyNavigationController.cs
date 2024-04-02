1f004f4e2062117453faa7bcb288d80237b5d61a
Ignacio Martínez Gallardo <ignamarga@gmail.com>
2024-04-02 13:42:50 +0200

    Added states + chase behaviour to the enemies

--- RobotPlatformer3D/Assets/Scripts/EnemyNavigationController.cs
+++ RobotPlatformer3D/Assets/Scripts/EnemyNavigationController.cs
@@ -9,11 +9,20 @@ public class EnemyNavigationController : MonoBehaviour
     public GameObject[] pathPositions;   
     public int actualObjective;
 
+    enum State { PATROL, CHASE, ATTACK };
+    State state;
+
+    public GameObject player;
+    private float playerDetectionRange = 15f;
+    private float rotationSpeed = 720;
+
     // Start is called before the first frame update
     void Start()
     {
         if (agent == null)
         {
+            state = State.PATROL;
+
             // The first path position the agent has to go is the 0
             agent = GetComponent<NavMeshAgent>();
             agent.SetDestination(pathPositions[0].transform.position);   
@@ -22,6 +31,24 @@ public class EnemyNavigationController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        switch (state)
+        {
+            case State.PATROL:
+                Patrol();
+                break;
+
+            case State.CHASE:
+                Chase();
+                break; 
+
+            case State.ATTACK:
+                Attack();
+                break;
+        }
+    }
+
+    private void Patrol()
     {
         // When the agent reaches the position
         if (agent.remainingDistance <= agent.stoppingDistance)
@@ -35,5 +62,45 @@ public class EnemyNavigationController : MonoBehaviour
             }
             agent.destination = pathPositions[actualObjective].transform.position;
         }
+
+        // Detecting the Player
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, playerDetectionRange))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                agent.speed = 20f;
+                state = State.CHASE;
+            }
+        }
+    }
+
+    private void Chase()
+    {
+        agent.SetDestination(player.transform.position);
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        // Calcula la rotación hacia la dirección del jugador
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+        // Suaviza la rotación hacia el jugador usando Quaternion.Lerp
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+
+        // Si el jugador está dentro del rango de ataque, cambia al estado de ataque
+        if (Vector3.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
+        {
+            state = State.ATTACK;
+        }
+
+        // Si el jugador está fuera del rango de detección, vuelve al estado de patrulla
+        if (Vector3.Distance(transform.position, player.transform.position) > playerDetectionRange)
+        {
+            state = State.PATROL;
+        }
+    }
+
+    private void Attack()
+    {
+
     }
 }
