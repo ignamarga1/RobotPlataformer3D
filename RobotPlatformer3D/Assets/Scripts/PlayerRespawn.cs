b49237bcda112c4e19aa28d5c00618e701bea149
Ignacio Martínez Gallardo <ignamarga@gmail.com>
2024-06-18 03:13:02 +0200

    decoration + bridge + chest mechanic

--- RobotPlatformer3D/Assets/Scripts/PlayerRespawn.cs
+++ RobotPlatformer3D/Assets/Scripts/PlayerRespawn.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    private Vector3 spawnPoint = new Vector3(0, 0, -110);
+    //private Vector3 spawnPoint = new Vector3(0, 0, -110); // Level 1
+    private Vector3 spawnPoint = new Vector3(100, 2, 12);   // Level 2
 
     // Update is called once per frame
     void FixedUpdate()
